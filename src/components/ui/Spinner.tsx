0084c31af521ab13b7a120b87f3ff707d05ed944
djuricmarko <markodjuric94@proton.me>
2025-04-24 21:11:19 +0200

    feat: adding swr for fetching

--- src/components/ui/Spinner.tsx
+++ src/components/ui/Spinner.tsx
@@ -1,6 +1,6 @@
 import { cn } from '~/lib/utils';
 
-export function LoadingSpinner({ className }: { className?: string | undefined}) {
+export function LoadingSpinner({ className }: { className?: string | undefined }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,4 +17,4 @@ export function LoadingSpinner({ className }: { className?: string | undefined})
       <path d="M21 12a9 9 0 1 1-6.219-8.56"/>
     </svg>
   );
-};
+}
