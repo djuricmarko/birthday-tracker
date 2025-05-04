89129479e81c284708ca1eac1097d0088f8167a3
djuricmarko <markodjuric94@proton.me>
2025-05-04 02:42:59 +0200

    feat: birthday types

--- src/services/birthday.ts
+++ src/services/birthday.ts
@@ -1,11 +1,17 @@
 import 'server-only';
 
 import { neon } from '@neondatabase/serverless';
+import { Birthday } from '~/types/birthdayTypes';
 
 const sql = neon(process.env.DATABASE_URL!);
 
-export async function getBirthdays() {
-  const birthdays = await sql`SELECT * FROM birthdays`
+/**
+ * Fetches all birthdays from the database.
+ * @returns A promise that resolves to an array of Birthday objects.
+ */
+export async function getBirthdays(): Promise<{ birthdays: Birthday[] }>{
+  const birthdays = await sql`SELECT * FROM birthdays` as Birthday[];
+
   return {
     birthdays
   }
