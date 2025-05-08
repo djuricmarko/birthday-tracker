78bf3f0ed8e90c0e624ae5c94bc42fe5faa85d3b
djuricmarko <markodjuric94@proton.me>
2025-05-09 01:07:58 +0200

    feat: adding suspense

--- src/app/(birthdays)/page.tsx
+++ src/app/(birthdays)/page.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from 'react';
 import Link from 'next/link';
 import type { Birthday } from '~/types/birthdayTypes';
 import { BirthdayList } from '~/app/(birthdays)/BirthdayList';
 import { getBirthdays } from '~/services/birthday';
 import { Button } from '~/components/ui/button';
 import { auth } from '@clerk/nextjs/server';
+import { LoadingSpinner } from '~/components/ui/Spinner';
 
 export default async function BirthdayTracker() {
   const { birthdays } = await getBirthdays();
@@ -20,7 +22,9 @@ export default async function BirthdayTracker() {
   return (
     <div className="flex flex-col justify-center items-center w-full pt-10">
       <div className="grid gap-8 w-full px-5 max-w-[500px]">
-        <BirthdayList birthdays={birthdays as Birthday[]}/>
+        <Suspense fallback={<LoadingSpinner className="justify-self-center"/>}>
+          <BirthdayList birthdays={birthdays as Birthday[]}/>
+        </Suspense>
         <Link href="/add" className="justify-self-center">
           <Button className="w-40 cursor-pointer" variant="outline">
             Add
