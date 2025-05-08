7f866286f06d9fc744054a54a058d77b220e67d5
djuricmarko <markodjuric94@proton.me>
2025-05-09 00:27:05 +0200

    feat: optimize protected pages

--- src/components/Layouts/MainLayout.tsx
+++ src/components/Layouts/MainLayout.tsx
@@ -1,20 +1,12 @@
 import type { ReactNode } from 'react';
 import Header from './Header';
-import { SignedIn, SignedOut } from '@clerk/nextjs';
 
 export function MainLayout({ children }: { children: ReactNode }) {
   return (
     <>
       <Header/>
       <main className="container mx-auto h-[calc(100vh-60px)]">
-        <SignedIn>
-          {children}
-        </SignedIn>
-        <SignedOut>
-          <div className="flex flex-col items-center justify-center h-full">
-            <h2>Uloguj se prvo</h2>
-          </div>
-        </SignedOut>
+        {children}
       </main>
     </>
   );
