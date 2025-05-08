b38dbbbe0b051af7a05d051029629e7259891693
djuricmarko <markodjuric94@proton.me>
2025-05-09 00:27:05 +0200

    feat: optimize protected pages

--- src/app/(birthdays)/page.tsx
+++ src/app/(birthdays)/page.tsx
@@ -3,9 +3,19 @@ import type { Birthday } from '~/types/birthdayTypes';
 import { BirthdayList } from '~/app/(birthdays)/BirthdayList';
 import { getBirthdays } from '~/services/birthday';
 import { Button } from '~/components/ui/button';
+import { auth } from '@clerk/nextjs/server';
 
 export default async function BirthdayTracker() {
   const { birthdays } = await getBirthdays();
+  const { userId } = await auth();
+
+  if (!userId) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen text-center px-4">
+        <h1>Login first</h1>
+      </div>
+    );
+  }
 
   return (
     <div className="flex flex-col justify-center items-center w-full pt-10">
