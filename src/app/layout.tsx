608f507f98f5e28c2a175e5643a3ae5abaeb566e
djuricmarko <markodjuric94@proton.me>
2025-05-09 22:49:26 +0200

    refactor: change app logo and font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,15 @@
 import type { ReactNode } from 'react';
 import type { Metadata } from 'next';
 import { ClerkProvider } from '@clerk/nextjs';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist } from 'next/font/google';
 import { ToastProvider } from '~/components/context/ToastContext';
 import { QueryProvider } from '~/components/context/QueryContext';
 import { MainLayout } from '~/components/Layouts/MainLayout';
 import '../styles/globals.css';
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
   subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  preload: true,
 });
 
 export const metadata: Metadata = {
@@ -29,7 +24,7 @@ export default function RootLayout({ children }: Readonly<{ children: ReactNode 
   return (
     <ClerkProvider>
       <html lang="sr">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${geistSans.className} antialiased`}>
       <ToastProvider>
         <QueryProvider>
           <MainLayout>
