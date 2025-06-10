61508b537d6294d9e11cb1fc2c19a31d86db23aa
djuricmarko <markodjuric94@proton.me>
2025-06-11 00:51:59 +0200

    feat: adding sidebar

--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,9 +1,14 @@
-import { SignedOut, SignInButton, SignUpButton } from '@clerk/nextjs';
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
+import { currentUser } from '@clerk/nextjs/server';
+
 import { Button } from '~/components/ui/button';
 import { SidebarTrigger } from '~/components/ui/sidebar';
+import { Skeleton } from '~/components/ui/skeleton';
 
 async function Header() {
-    return (
+  const user = await currentUser();
+
+  return (
     <header className="h-[50px] border-b border-[#F3F3F3]">
       <div className="flex justify-between items-center gap-4 h-full px-5">
         <div className="flex items-center gap-4">
@@ -21,6 +26,13 @@ async function Header() {
               <Button className="cursor-pointer">Register</Button>
             </SignUpButton>
           </SignedOut>
+          <SignedIn>
+            <div className="flex flex-col items-end justify-center">
+              <p className="text-xs">{user?.fullName}</p>
+              <p className="text-xs text-gray-600">{user?.emailAddresses?.[0]?.emailAddress}</p>
+            </div>
+            <UserButton fallback={<Skeleton className="bg-gray-300 w-[28px] h-[28px] rounded-full"/>}/>
+          </SignedIn>
         </div>
       </div>
     </header>
